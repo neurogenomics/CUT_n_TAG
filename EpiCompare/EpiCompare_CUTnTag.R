2c37b00c56efdfebdf4acd416f5532b9bba46256
Sera Choi <serachoi1230@gmail.comwq>
2022-03-04 12:03:35 +0000

    Added more samples, updated EpiCompare report

--- EpiCompare/EpiCompare_CUTnTag.R
+++ EpiCompare/EpiCompare_CUTnTag.R
@@ -1,42 +1,61 @@
-# Analysis of CnT, ENCODE and TIP-seq using EpiCompare
+## EpiCompare analysis of CUT&Tag, CUT&Run, ENCODE and TIP-seq
 
-# CnT peakfiles
-active_seacr <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peak_files/ActiveMotif_SEACR.bed", as="GRanges")
-active_macs <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peak_files/ActiveMotif_MACS2.bed", as="GRanges")
-abcam_seacr <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peak_files/Abcam-ab4729_SEACR.bed", as="GRanges")
-abcam_macs <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peak_files/Abcam-ab4729_MACS2.bed", as="GRanges")
-CnT_seacr <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peak_files/H3K27ac_SRR8383507_SEACR.bed", as="GRanges")
-CnT_macs <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peak_files/H3K27ac_SRR8383507_MACS2.bed", as="GRanges")
+## Load peakfiles in BED format
+# CUT&Tag
+active_seacr <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peakfiles/CnT/ActiveMotif_SEACR.bed", as="GRanges")
+active_macs <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peakfiles/CnT/ActiveMotif_MACS2.bed", as="GRanges")
+abcam_seacr <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peakfiles/CnT/Abcam-ab4729_SEACR.bed", as="GRanges")
+abcam_macs <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peakfiles/CnT/Abcam-ab4729_MACS2.bed", as="GRanges")
+kayaOkur_seacr <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peakfiles/CnT/H3K27ac_SRR8383507_SEACR.bed", as="GRanges")
+kayaOkur_macs <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peakfiles/CnT/H3K27ac_SRR8383507_MACS2.bed", as="GRanges")
 
-# ENCODE peakfiles
-H3K27me3_seacr <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peak_files/H3K27me3_SEACR.bed", as="GRanges")
-H3K27me3_macs <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peak_files/H3K27me3_MACS2.bed", as="GRanges")
-encode_H3K27ac_full <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/ENCODE_H3K27ac.bed", as="GRanges")
+# CUT&Run
+meers_ac_macs <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peakfiles/CnT/H3K27ac_SRR8581604_CnR_MACS2.bed", as="GRanges")
+meers_ac_seacr <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peakfiles/CnT/H3K27ac_SRR8581604_CnR_SEACR.bed", as="GRanges")
+meers_me_macs <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peakfiles/CnT/H3K27me3_SRR9073702_CnR_MACS2.bed", as="GRanges")
+meers_me_seacr <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peakfiles/CnT/H3K27me3_SRR9073702_CnR_SEACR.bed", as="GRanges")
 
-# TIP-seq peakfiles
-tip_R1 <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/tip_peakfiles/S_1_R1.peaks.bed.stringent.bed", as="GRanges")
-tip_R2 <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/tip_peakfiles/S_1_R2.peaks.bed.stringent.bed", as="GRanges")
-tip <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/tip_peakfiles/S_4_R1.peaks.bed.stringent.bed", as="GRanges")
+# ENCODE
+encode_H3K27me <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peakfiles/ENCODE/ENCODE_H3K27me3.bed", as="GRanges")
+encode_H3K27ac <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peakfiles/ENCODE/ENCODE_H3K27ac.bed", as="GRanges")
 
+# TIP-seq
+tip_1_05_01 <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peakfiles/TIP/S_1_05_01_22_R1.peaks.bed.stringent.bed", as="GRanges")
+tip_2_28_01 <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peakfiles/TIP/S_2_28_01_22_R1.peaks.bed.stringent.bed", as="GRanges")
+tip_3_28_01 <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peakfiles/TIP/S_3_28_01_22_R1.peaks.bed.stringent.bed", as="GRanges")
+tip_4_03_02 <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peakfiles/TIP/S_4_03_02_22_R1.peaks.bed.stringent.bed", as="GRanges")
+tip_4_28_01 <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peakfiles/TIP/S_4_28_01_22_R1.peaks.bed.stringent.bed", as="GRanges")
+tip_5_28_01 <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peakfiles/TIP/S_5_28_01_22_R1.peaks.bed.stringent.bed", as="GRanges")
+tip_6_28_01 <- ChIPseeker::readPeakFile("/Users/serachoi/Documents/EpiCompare_extra/peakfiles/TIP/S_6_28_01_22_R1.peaks.bed.stringent.bed", as="GRanges")
 
-# peaklist
-peaklist <- list(active_macs, active_seacr, abcam_macs, abcam_seacr, CnT_macs, CnT_seacr, H3K27me3_macs, H3K27me3_seacr, encode_H3K27ac_full, tip_R1, tip_R2, tip)
-names(peaklist) <- c("H3K27ac_CnT_ActiveMotif_MACS2", "H3K27ac_CnT_ActiveMotif_SEACR",
-                     "H3K27ac_CnT_Abcam-ab4729_MACS2", "H3K27ac_CnT_Abcam-ab4729_SEACR",
-                     "H3K27ac_CnT_SRR8383507_MACS2", "H3K27ac_CnT_SRR8383507_SEACR",
-                     "H3K27me3_ENCODE_MACS2.bed", "H3K27me3_ENCODE_SEACR.bed",
-                     "H3K27ac_ENCODE",
-                     "H3K27ac_TIP_Abcam.phase_1_05_jan_2022.S_1_R1", "H3K27ac_TIP_Abcam.phase_1_05_jan_2022.S_1_R2",
-                     "H3K27ac_TIP_Abcam.phase_2_03_feb_2022.S_4_R1")
+# create peaklist
+peaklist <- list(active_seacr, active_macs, abcam_seacr, abcam_macs, kayaOkur_seacr, kayaOkur_macs,
+                 meers_ac_seacr, meers_ac_macs, meers_me_seacr, meers_me_macs,
+                 encode_H3K27me, encode_H3K27ac,
+                 tip_1_05_01, tip_4_03_02, tip_2_28_01, tip_3_28_01, tip_4_28_01, tip_5_28_01, tip_6_28_01)
+# set names
+my_label <- c("H3K27ac_CnT_ActiveMotif_SEACR", "H3K27ac_CnT_ActiveMotif_MACS2",
+              "H3K27ac_CnT_Abcamab4729_SEACR", "H3K27ac_CnT_Abcamab4729_MACS2",
+              "H3K27ac_CnT_KayaOkur_SEACR", "H3K27ac_CnT_KayaOkur_MACS2",
+              "H3K27ac_CnR_Meers_SEACR", "H3K27ac_CnR_Meers_MACS2",
+              "H3K27me3_CnR_Meers_SEACR", "H3K27me3_CnR_Meers_MACS2",
+              "H3K27me3_ENCODE", "H3K27ac_ENCODE",
+              "H3K27ac_TIP_Abcam.phase_1_05_jan_2022.S_1_R1",
+              "H3K27ac_TIP_Abcam.phase_2_03_feb_2022.S_4_R1",
+              "H3K27ac_TIP_Diagenode.phase_2_28_jan_2022.S_2_R1",
+              "H3K27me3_TIP_Diagenode.phase_2_28_jan_2022.S_3_R1",
+              "H3K27ac_TIP_Diagenode.phase_2_28_jan_2022.S_4_R1",
+              "H3K27me3_TIP_Diagenode.phase_2_28_jan_2022.S_5_R1",
+              "H3K27ac_TIP_Diagenode.phase_2_28_jan_2022.S_6_R1")
+names(peaklist) <- my_label
 
-# reference peakfile: ENCODE H3K27ac (data accession: ENCFF044JNJ)
-reference <- encode_H3K27ac_full
-names(reference) <- "ENCODE_H3K27ac"
-# blacklist
+# reference file
+reference <- list("ENCODE_H3K27ac" = encode_H3K27ac)
+# hg19 blacklist
 data("hg19_blacklist")
 
-# EpiCompare
-EpiCompare::EpiCompare(peakfiles = peaklist,
+# Epicompare
+EpiCompare(peakfiles = peaklist,
            blacklist = hg19_blacklist,
            reference = reference,
            stat_plot = TRUE,
@@ -47,5 +66,6 @@ EpiCompare::EpiCompare(peakfiles = peaklist,
            enrichment_plot = TRUE,
            interact = TRUE,
            output_dir = "/Users/serachoi/Documents/EpiCompare")
+
 
 
